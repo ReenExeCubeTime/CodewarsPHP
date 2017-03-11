01d87784fdebf5d1615d7e05932b8aafa35052fa
ReenExe <ipourri@gmail.com>
2017-03-12 01:23:32 +0200

    pass tests (#6)

--- src/StringLetterReverser.php
+++ src/StringLetterReverser.php
@@ -4,7 +4,16 @@ namespace ReenExe\CodewarsPHP;
 
 class StringLetterReverser
 {
+    /**
+     * @param $string
+     * @return string
+     */
     public function reverse($string)
     {
+        $parts = explode(' ', $string);
+
+        $reversed = array_map('strrev', $parts);
+
+        return implode(' ', $reversed);
     }
 }
