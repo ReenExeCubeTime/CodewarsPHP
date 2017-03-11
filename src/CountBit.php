bfce3f87e832bbf1d04c6de4d39fe29a9eb5e597
ReenExe <ipourri@gmail.com>
2017-03-11 21:20:43 +0200

    pass tests (#1)

--- src/CountBit.php
+++ src/CountBit.php
@@ -4,8 +4,17 @@ namespace ReenExe\CodewarsPHP;
 
 class CountBit
 {
+    /**
+     * @param $number
+     * @return int
+     */
     public function calculate($number)
     {
-
+        $result = 0;
+        while ($number) {
+            $result += $number % 2;
+            $number = $number >> 1;
+        }
+        return $result;
     }
 }
