6ff8d1aff53dacf666908d140039aaeff2dff827
ReenExe <ipourri@gmail.com>
2017-03-11 22:03:03 +0200

    prepare code for solve (#4)

---
+++ src/Benefactor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReenExe\CodewarsPHP;
+
+class Benefactor
+{
+    public function average(array $array, $new)
+    {
+
+    }
+}
--- tests/BenefactorTest.php
+++ tests/BenefactorTest.php
@@ -4,17 +4,23 @@ namespace ReenExe\CodewarsPHP\Tests;
 
 use Doctrine\Instantiator\Exception\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use ReenExe\CodewarsPHP\Benefactor;
 
 class BenefactorTest extends TestCase
 {
-    public function testRainfallBasics() {
-        $this->assertSame(newAvg([14, 30, 5, 7, 9, 11, 15], 92), 645);
-        $this->assertSame(newAvg([14, 30, 5, 7, 9, 11, 16], 90), 628);
+    public function testRainfallBasics()
+    {
+        $benefactor = new Benefactor();
+        $this->assertSame(645, $benefactor->average([14, 30, 5, 7, 9, 11, 15], 92));
+        $this->assertSame(628, $benefactor->average([14, 30, 5, 7, 9, 11, 16], 90));
     }
+
     /**
      * @expectedException InvalidArgumentException
      */
-    public function testException() {
-        $test = newAvg([14, 30, 5, 7, 9, 11, 15], 2);
+    public function testException()
+    {
+        $benefactor = new Benefactor();
+        $benefactor->average([14, 30, 5, 7, 9, 11, 15], 2);
     }
 }
