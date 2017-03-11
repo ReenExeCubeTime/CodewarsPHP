96c30d86f2fc6dab3ecc8b473fbf7975707fbb6b
ReenExe <ipourri@gmail.com>
2017-03-11 21:39:11 +0200

    pass tests (#2)

--- src/ComplementaryDNA.php
+++ src/ComplementaryDNA.php
@@ -4,8 +4,19 @@ namespace ReenExe\CodewarsPHP;
 
 class ComplementaryDNA
 {
+    /**
+     * @param string $dna
+     * @return string
+     */
     public function strand($dna)
     {
+        $map = [
+            'T' => 'A',
+            'A' => 'T',
+            'C' => 'G',
+            'G' => 'C',
+        ];
 
+        return strtr($dna, $map);
     }
 }
