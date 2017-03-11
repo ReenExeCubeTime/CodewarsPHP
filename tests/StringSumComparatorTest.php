20bd917d4b956bedb05e62a0921d7c80f44944ef
ReenExe <ipourri@gmail.com>
2017-03-11 23:22:41 +0200

    covers (#5)

--- tests/StringSumComparatorTest.php
+++ tests/StringSumComparatorTest.php
@@ -12,6 +12,7 @@ class StringSumComparatorTest extends TestCase
         $comparator = new StringSumComparator();
         $this->assertSame(true, $comparator->compare('AD', 'BC'));
         $this->assertSame(true, $comparator->compare('abc', 'CBA'));
+        $this->assertSame(true, $comparator->compare('ZzZz', 'ffPFF'));
         $this->assertSame(true, $comparator->compare('!!', '7476'));
     }
 }
