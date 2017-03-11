d930122062f080f331cebe315d3cecfea6a4317b
ReenExe <ipourri@gmail.com>
2017-03-11 23:12:29 +0200

    Compare Strings by Sum of Chars #5

---
+++ tests/StringSumComparatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReenExe\CodewarsPHP\Tests;
+
+class StringSumComparatorTest
+{
+    public function testExample()
+    {
+        $this->assertSame(true, compare("AD", "BC"));
+    }
+}
