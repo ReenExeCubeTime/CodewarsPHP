3cd5fb4df4096cb83df11a67780bad7037f4d442
ReenExe <ipourri@gmail.com>
2017-03-13 01:27:44 +0200

    prepare (#10)

--- src/ScalingSquaredString.php
+++ src/ScalingSquaredString.php
@@ -4,5 +4,8 @@ namespace ReenExe\CodewarsPHP;
 
 class ScalingSquaredString
 {
+    public function scale($string, $x, $y)
+    {
 
+    }
 }
--- tests/ScalingSquaredStringTest.php
+++ tests/ScalingSquaredStringTest.php
@@ -3,15 +3,17 @@
 namespace ReenExe\CodewarsPHP\Tests;
 
 use PHPUnit\Framework\TestCase;
+use ReenExe\CodewarsPHP\ScalingSquaredString;
 
 class ScalingSquaredStringTest extends TestCase
 {
     public function testScalingBasics()
     {
+        $scaling = new ScalingSquaredString();
         $a = "abcd\nefgh\nijkl\nmnop";
         $r = "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\niijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp";
-        $this->assertSame(scale($a, 2, 3), $r);
+        $this->assertSame($scaling->scale($a, 2, 3), $r);
 
-        $this->assertSame(scale("CG\nla", 2, 3), "CCGG\nCCGG\nCCGG\nllaa\nllaa\nllaa");
+        $this->assertSame($scaling->scale("CG\nla", 2, 3), "CCGG\nCCGG\nCCGG\nllaa\nllaa\nllaa");
     }
 }
