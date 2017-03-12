ab40bc0d9b4ff11796c99ec39047b9397bae2d13
ReenExe <ipourri@gmail.com>
2017-03-13 01:38:02 +0200

    pass tests (#10)

--- src/ScalingSquaredString.php
+++ src/ScalingSquaredString.php
@@ -6,6 +6,30 @@ class ScalingSquaredString
 {
     public function scale($string, $x, $y)
     {
+        if ($string === '') {
+            return '';
+        }
 
+        $lines = explode(\PHP_EOL, $string);
+
+        $result = [];
+
+        foreach ($lines as $line) {
+            $letterList = [];
+
+            $length = strlen($line);
+
+            for ($i = 0; $i < $length; ++$i) {
+                $letterList[] = str_repeat($line[$i], $x);
+            }
+
+            $resultLine = implode('', $letterList);
+
+            for ($i = 0; $i < $y; ++$i) {
+                $result[] = $resultLine;
+            }
+        }
+
+        return implode(\PHP_EOL, $result);
     }
 }
