c79e5fcf2f391d78c0653740a06a315a5a4e7d15
ReenExe <ipourri@gmail.com>
2017-03-12 02:32:30 +0200

    pass test as first time run (#8)

--- src/FibonacciSequence.php
+++ src/FibonacciSequence.php
@@ -4,8 +4,22 @@ namespace ReenExe\CodewarsPHP;
 
 class FibonacciSequence
 {
-    public function calculate($signature, $count)
+    public function calculate(array $signature, $n)
     {
+        $step = count($signature);
 
+        if ($n <= $step) {
+            return array_slice($signature, 0, $n);
+        }
+
+        $rest = $n - $step;
+
+        for ($index = 0; $index < $rest; ++$index) {
+            $values = array_slice($signature, -$step);
+
+            $signature[] = array_sum($values);
+        }
+
+        return $signature;
     }
 }
