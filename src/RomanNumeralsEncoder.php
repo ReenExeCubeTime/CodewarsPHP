2864250fddef6b5b79a490290fafdb9dc1b94fc0
ReenExe <ipourri@gmail.com>
2017-03-14 01:17:12 +0200

    copy map (#21)

--- src/RomanNumeralsEncoder.php
+++ src/RomanNumeralsEncoder.php
@@ -6,5 +6,14 @@ class RomanNumeralsEncoder
 {
     public function encode($number)
     {
+        $map = [
+            1 => 'I',
+            5 => 'V',
+            10 => 'X',
+            50 => 'L',
+            100 => 'C',
+            500 => 'D',
+            1000 => 'M',
+        ];
     }
 }
