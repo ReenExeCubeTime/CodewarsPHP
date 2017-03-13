937e183862a0a4741f6c176e119f1b7bb5e20ea2
ReenExe <ipourri@gmail.com>
2017-03-13 02:35:26 +0200

    prepare (#14)

---
+++ src/FindMissingLetter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReenExe\CodewarsPHP;
+
+class FindMissingLetter
+{
+    public function find(array $letters)
+    {
+
+    }
+}
--- tests/FindTheMissingLetterTest.php
+++ tests/FindTheMissingLetterTest.php
@@ -3,12 +3,14 @@
 namespace ReenExe\CodewarsPHP\Tests;
 
 use PHPUnit\Framework\TestCase;
+use ReenExe\CodewarsPHP\FindMissingLetter;
 
 class FindTheMissingLetterTest extends TestCase
 {
     public function testDescriptionExamples()
     {
-        $this->assertSame('e', find_missing_letter(['a', 'b', 'c', 'd', 'f']));
-        $this->assertSame('P', find_missing_letter(['O', 'Q', 'R', 'S']));
+        $finder = new FindMissingLetter();
+        $this->assertSame('e', $finder->find(['a', 'b', 'c', 'd', 'f']));
+        $this->assertSame('P', $finder->find(['O', 'Q', 'R', 'S']));
     }
 }
