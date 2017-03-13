0bc31cbc7297b1a4ea0e57fe350fed8060f83b80
ReenExe <ipourri@gmail.com>
2017-03-13 22:29:53 +0200

    pass tests (#15)

--- src/OrganiseDuplicateNumbers.php
+++ src/OrganiseDuplicateNumbers.php
@@ -4,7 +4,19 @@ namespace ReenExe\CodewarsPHP;
 
 class OrganiseDuplicateNumbers
 {
-    public function group(array $input)
+    public function group(array $numbers)
     {
+        $map = array_fill_keys($numbers, 0);
+
+        foreach ($numbers as $number) {
+            $map[$number] += 1;
+        }
+
+        $result = [];
+        foreach ($map as $number => $count) {
+            $result[] = array_fill(0, $count, $number);
+        }
+
+        return $result;
     }
 }
