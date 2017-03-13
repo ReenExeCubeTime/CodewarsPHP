9383c0de41e3cfbc0d3bbec07a49e59154b977ef
ReenExe <ipourri@gmail.com>
2017-03-14 01:03:29 +0200

    pass tests (#20)

--- src/CamelCaseMethod.php
+++ src/CamelCaseMethod.php
@@ -6,6 +6,8 @@ class CamelCaseMethod
 {
     public function convert(string $string): string
     {
-
+        $words = explode(' ', $string);
+        $upper = array_map('ucfirst', $words);
+        return implode('', $upper);
     }
 }
