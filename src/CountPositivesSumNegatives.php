87ad4d0270ac8627c002c70db3d04eb95a5d65bb
ReenExe <ipourri@gmail.com>
2017-03-13 02:29:50 +0200

    pass tests (#13)

--- src/CountPositivesSumNegatives.php
+++ src/CountPositivesSumNegatives.php
@@ -6,6 +6,17 @@ class CountPositivesSumNegatives
 {
     public function sum(array $numbers)
     {
+        $positive = 0;
+        $negative = 0;
 
+        foreach ($numbers as $number) {
+            if ($number > 0) {
+                ++$positive;
+            } else {
+                $negative += $number;
+            }
+        }
+
+        return [$positive, $negative];
     }
 }
