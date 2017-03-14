5b49aa78a749fc08afd9c88e7f30040d1474cbb5
ReenExe <ipourri@gmail.com>
2017-03-14 22:19:30 +0200

    pass tests (#21)

--- src/RomanNumeralsEncoder.php
+++ src/RomanNumeralsEncoder.php
@@ -4,16 +4,35 @@ namespace ReenExe\CodewarsPHP;
 
 class RomanNumeralsEncoder
 {
-    public function encode($number)
+    public function encode($from)
     {
         $map = [
-            1 => 'I',
-            5 => 'V',
-            10 => 'X',
-            50 => 'L',
-            100 => 'C',
-            500 => 'D',
             1000 => 'M',
+            900 => 'CM',
+            500 => 'D',
+            400 => 'CD',
+            100 => 'C',
+            90 => 'XC',
+            50 => 'L',
+            40 => 'XL',
+            10 => 'X',
+            9 => 'IX',
+            5 => 'V',
+            4 => 'IV',
+            1 => 'I',
         ];
+
+        $list = [];
+
+        foreach ($map as $number => $value) {
+            $multi = (int)($from / $number);
+            if ($multi) {
+                $list[] = str_repeat($value, $multi);
+
+                $from %= $number;
+            }
+        }
+
+        return implode('', $list);
     }
 }
