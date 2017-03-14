d269b3f40ba6def53ccc0bd265f29964a8515727
ReenExe <ipourri@gmail.com>
2017-03-15 00:48:11 +0200

    pass tests (#23)

---
+++ src/SumStrings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReenExe\CodewarsPHP;
+
+class SumStrings
+{
+    public function sum($a, $b)
+    {
+        return (string)($a + $b);
+    }
+}
--- tests/SumStringsTest.php
+++ tests/SumStringsTest.php
@@ -3,11 +3,13 @@
 namespace ReenExe\CodewarsPHP\Tests;
 
 use PHPUnit\Framework\TestCase;
+use ReenExe\CodewarsPHP\SumStrings;
 
 class SumStringsTest extends TestCase
 {
     public function testExamples()
     {
-        $this->assertSame('579', sum_strings('123', '456'));
+        $summer = new SumStrings();
+        $this->assertSame('579', $summer->sum('123', '456'));
     }
 }
