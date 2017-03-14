6e595d21bfcdaca8c094f4c70d7424061be0e640
ReenExe <ipourri@gmail.com>
2017-03-15 00:15:39 +0200

    pass tests (#22) like class FibonacciSequence

--- src/FibonacciSquarePerimeter.php
+++ src/FibonacciSquarePerimeter.php
@@ -6,5 +6,14 @@ class FibonacciSquarePerimeter
 {
     public function calculate($n)
     {
+        $sequence = [1, 1];
+
+        for ($index = 0; $index < $n - 1; ++$index) {
+            $values = array_slice($sequence, -2);
+
+            $sequence[] = array_sum($values);
+        }
+
+        return array_sum($sequence) * 4;
     }
 }
