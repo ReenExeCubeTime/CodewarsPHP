efc9beec0da5c43bbf9dd2a9c258980eaf62b3c4
ReenExe <ipourri@gmail.com>
2017-03-15 03:42:09 +0200

    pass tests (#25)

--- src/GoingFactorialSequence.php
+++ src/GoingFactorialSequence.php
@@ -6,5 +6,14 @@ class GoingFactorialSequence
 {
     public function calculate($n)
     {
+        $factorial = 1;
+        $sum = $factorial;
+
+        for ($i = 2; $i <= $n; ++$i) {
+            $factorial *= $i;
+            $sum += $factorial;
+        }
+
+        return floor($sum * 1e6 / $factorial) / 1e6;
     }
 }
