5db6e8fd74e0d4d67245e80e8ccb1c2e2e7ea138
ReenExe <ipourri@gmail.com>
2017-03-15 03:20:55 +0200

    pass tests (#24)

--- src/PascalsTriangle.php
+++ src/PascalsTriangle.php
@@ -6,6 +6,36 @@ class PascalsTriangle
 {
     public function calculate($n)
     {
-        
+        $step = [1];
+
+        if ($n === 1) {
+            return $step;
+        }
+
+        $result = [$step];
+
+        for ($index = 2; $index <= $n; ++$index) {
+            $left = $step;
+            $right = $step;
+            array_unshift($left, 0);
+            array_push($right, 0);
+            $step = $this->sum($left, $right);
+            $result[] = $step;
+        }
+
+        return array_merge(...$result);
+    }
+
+    private function sum(array $a, array $b)
+    {
+        $count = count($a);
+
+        $sum = [];
+
+        for ($i = 0; $i < $count; ++$i) {
+            $sum[] = $a[$i] + $b[$i];
+        }
+
+        return $sum;
     }
 }
