a11c2b18a4df40c3aa924e148100c826e4941521
ReenExe <ipourri@gmail.com>
2017-03-16 23:48:57 +0200

    cleanup (#29)

--- src/IPValidator.php
+++ src/IPValidator.php
@@ -4,7 +4,7 @@ namespace ReenExe\CodewarsPHP;
 
 class IPValidator
 {
-    public function valid($ip)
+    public function valid(string $ip): bool
     {
         return (bool)filter_var($ip, \FILTER_VALIDATE_IP);
     }
